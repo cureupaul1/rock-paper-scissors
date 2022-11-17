6bb20c055d1fb2c9616eadffdd7d1f2e6fd1873e
cureupaul1 <105922667+cureupaul1@users.noreply.github.com>
2022-11-17 06:00:20 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,48 @@
+
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".btn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click",() =>{
+    
+    player = button.textContent;
+    getComputherChoice();
+    playerText.textContent = "Player: " + player;
+    computerText.textContent = "Computer: " + computer;
+    resultText.textContent = "Result: " + checkWinner();
+}))
+
+function getComputherChoice(){
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+    switch(randomNum){
+        case 1:
+            computer = "Rock";
+            break;
+        case 2:
+            computer = "Paper";
+            break;
+        case 3:
+            computer = "Scissors";
+            break;
+        
+    }
+
+}
+function checkWinner() {
+    if(player == computer){
+        return "Draw!"
+    }
+    else if(computer == "Rock"){
+        return (player == "Paper") ? "You win" : "You lose";
+    }
+    else if(computer == "Paper"){
+        return (player == "Scissors") ? "You win" : "You lose";
+    }
+    else if(computer == "Scissors"){
+        return (player == "Rock") ? "You win" : "You lose";
+    }
+  }
